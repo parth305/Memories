344a09685c371e0f0073cb8bfc5def9c3655185e
parth <parth305pandya@gmail.com>
2022-04-22 23:14:20 +0530

    post added to the frontend

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -2,17 +2,23 @@ import React from 'react'
 import { useSelector } from 'react-redux';
 import Post from './Post/Post'
 import useStyles from "./styles";
+import {Grid,CircularProgress} from "@material-ui/core"
+// import moment from "moment"
 function Posts() {
     let classes=useStyles();
     let posts=useSelector((state)=>state.post);
     console.log(posts);
 
   return (
-    <div>
-      this is Posts
-      <Post/>
-      <Post/>
-    </div>
+    !posts.length?<CircularProgress/>:
+  
+    <Grid className={classes.mainContainer} container alignItems='stretch' spacing={3}>
+      {posts.map((post)=>(
+        <Grid item key={post._id} xs={12} sm={6}>
+          <Post post={post}/>
+        </Grid>
+      ))}
+    </Grid>
   )
 }
 
